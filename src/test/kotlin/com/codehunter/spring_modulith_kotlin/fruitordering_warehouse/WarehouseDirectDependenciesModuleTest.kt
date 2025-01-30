7454371186913c92468eaf09828fe60b36683952
hvhai <hunterx6323@gmail.com>
2025-01-30 14:59:47 +0700

    test: [Fruit-Ordering] add warehouse direct dependencies test

--- src/test/kotlin/com/codehunter/spring_modulith_kotlin/fruitordering_warehouse/WarehouseDirectDependenciesModuleTest.kt
+++ src/test/kotlin/com/codehunter/spring_modulith_kotlin/fruitordering_warehouse/WarehouseDirectDependenciesModuleTest.kt
@@ -4,16 +4,14 @@ import com.codehunter.spring_modulith_kotlin.ContainerBaseMoudulithTest
 import com.codehunter.spring_modulith_kotlin.TestInfra
 import com.codehunter.spring_modulith_kotlin.TestSecurityConfiguration
 import com.codehunter.spring_modulith_kotlin.WiremockInitializer
-import com.codehunter.spring_modulith_kotlin.eventsourcing.EventSourcingService
 import com.codehunter.spring_modulith_kotlin.eventsourcing.WarehouseEvent
 import com.codehunter.spring_modulith_kotlin.fruitordering_warehouse.internal.JpaWarehouseProduct
 import com.codehunter.spring_modulith_kotlin.fruitordering_warehouse.internal.WarehouseProductRepository
 import com.codehunter.spring_modulith_kotlin.share.OrderDTO
 import com.codehunter.spring_modulith_kotlin.share.OrderStatus
 import com.codehunter.spring_modulith_kotlin.share.ProductDTO
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
@@ -21,35 +19,31 @@ import org.springframework.modulith.test.ApplicationModuleTest
 import org.springframework.modulith.test.Scenario
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.bean.override.mockito.MockitoBean
+import java.util.function.Consumer
 import kotlin.test.assertEquals
 
-
-@ApplicationModuleTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ApplicationModuleTest(
+    mode = ApplicationModuleTest.BootstrapMode.DIRECT_DEPENDENCIES,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 @ContextConfiguration(initializers = arrayOf(WiremockInitializer::class))
 @Import(value = arrayOf(TestSecurityConfiguration::class, TestInfra::class))
 @ActiveProfiles("integration")
-class WarehouseStandaloneIntegrationTest : ContainerBaseMoudulithTest() {
+class WarehouseDirectDependenciesModuleTest : ContainerBaseMoudulithTest() {
     @Autowired
     lateinit var warehouseService: WarehouseService
 
     @Autowired
     lateinit var warehouseProductRepository: WarehouseProductRepository
 
-    @MockitoBean
-    lateinit var eventSourcingService: EventSourcingService
-
-
     @Test
-    fun `when reserve a product for order then product quantity  in stock decrease by 1`(scenario: Scenario?) {
+    fun `when reserve a product for order then product quantity  in stock decrease by 1`(scenario: Scenario) {
         // given
         // Stock: Apple: 10
         val allProduct: List<JpaWarehouseProduct> = warehouseProductRepository.findAll()
-        Assertions.assertThat<JpaWarehouseProduct>(allProduct).hasSize(3)
+        assertThat(allProduct).hasSize(3)
         val warehouseProductDTO1: JpaWarehouseProduct = allProduct.find { it.name == "Apple" }!!
         assertEquals(10, warehouseProductDTO1.quantity)
-
-        doNothing().`when`(eventSourcingService).addWarehouseEvent(any())
 
         // when
         val productDTO = ProductDTO(
@@ -64,20 +58,18 @@ class WarehouseStandaloneIntegrationTest : ContainerBaseMoudulithTest() {
             null,
             mutableSetOf(productDTO)
         )
-        warehouseService.reserveProductForOrder(orderDTO)
 
         // then
-        // Stock: Apple: 9
-        val updatedProduct: JpaWarehouseProduct =
-            warehouseProductRepository.findById(warehouseProductDTO1.id!!).get()
-        assertEquals(9, updatedProduct.quantity)
-        verify(eventSourcingService, times(4)).addWarehouseEvent(any())
-
-        argumentCaptor<WarehouseEvent>().apply {
-            verify(eventSourcingService, times(4)).addWarehouseEvent(capture())
-            assertEquals(4, allValues.size)
-            assertEquals(orderDTO.id, lastValue.orderId)
-            assertEquals(WarehouseEvent.WarehouseEventType.RESERVE_COMPLETED, lastValue.warehouseEventType)
-        }
+        scenario.stimulate(Runnable { warehouseService.reserveProductForOrder(orderDTO) })
+            .andWaitForEventOfType(WarehouseEvent::class.java)
+            .matching({
+                it.orderId == orderDTO.id
+                        && it.warehouseEventType == WarehouseEvent.WarehouseEventType.RESERVE_COMPLETED
+            })
+            .toArriveAndVerify(Consumer<WarehouseEvent> { _: WarehouseEvent ->
+                // Stock: Apple: 9
+                val updatedProduct = warehouseProductRepository.findById(warehouseProductDTO1.id!!).get()
+                assertThat(updatedProduct.quantity).isEqualTo(9)
+            })
     }
 }
