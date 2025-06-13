2f4e0585948434be10fe9627a2bec101cfeba783
hvhai <hunterx6323@gmail.com>
2025-06-13 14:14:10 +0700

    feat: show load when product warehouse updated

--- src/main/kotlin/com/codehunter/spring_modulith_kotlin/fruitordering_warehouse/internal/Jpa.kt
+++ src/main/kotlin/com/codehunter/spring_modulith_kotlin/fruitordering_warehouse/internal/Jpa.kt
@@ -1,10 +1,14 @@
 package com.codehunter.spring_modulith_kotlin.fruitordering_warehouse.internal
 
 import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
 import org.slf4j.LoggerFactory
+import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.math.BigDecimal
+import java.time.Instant
+
 
 class ProductOutOfStockException(message: String?, val product: JpaWarehouseProduct) : Exception(message)
 
@@ -14,6 +18,23 @@ class JpaListener {
     @PostPersist
     private fun afterCreate(product: JpaWarehouseProduct) {
         log.info("[PostPersist] JpaWarehouseProduct create product {}", product)
+    }
+
+    @PostLoad
+    private fun postLoad(product: JpaWarehouseProduct) {
+        product.originalQuantity = product.quantity
+    }
+
+    @PreUpdate
+    private fun preUpdate(product: JpaWarehouseProduct) {
+        val oldQuantity = product.originalQuantity
+        val newQuantity = product.quantity
+        log.info(
+            "[PreUpdate] JpaWarehouseProduct quantity changed for product {} from {} to {}",
+            product.id, oldQuantity, newQuantity
+        )
+        // Update the originalQuantity for future updates if needed
+        product.originalQuantity = newQuantity
     }
 }
 
@@ -29,6 +50,9 @@ data class JpaWarehouseProduct(
     val quantity: Int,
     val price: BigDecimal
 ) {
+    @Transient
+    var originalQuantity: Int? = null
+
     override fun toString(): String {
         return "JpaWarehouseProduct{" +
                 "id='" + id + '\'' +
@@ -50,3 +74,18 @@ data class JpaWarehouseProduct(
 interface WarehouseProductRepository : JpaRepository<JpaWarehouseProduct, String> {
     fun findByName(name: String): JpaWarehouseProduct?
 }
+
+@Entity
+@Table(name = "fruit_warehouse_product_history")
+data class JpaWarehouseProductHistory(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    @Column(name = "product_id")
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    val productId: String,
+    val oldQuantity: Int,
+    val newQuantity: Int,
+    @CreationTimestamp
+    val createAt:Instant
+)
