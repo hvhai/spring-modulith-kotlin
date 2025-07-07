ea22d0a982231071ebef8e5b37f0520e67ed1d91
hvhai <hunterx6323@gmail.com>
2025-07-07 23:09:32 +0700

    feat: PERS-48 increase timeout

--- src/test/kotlin/com/codehunter/spring_modulith_kotlin/todo/TodoIntegrationTest.kt
+++ src/test/kotlin/com/codehunter/spring_modulith_kotlin/todo/TodoIntegrationTest.kt
@@ -35,7 +35,7 @@ import kotlin.test.assertNull
     classes = arrayOf(SpringModulithKotlinApplication::class),
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
-@DirtiesContext
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 @ContextConfiguration(initializers = arrayOf(WiremockInitializer::class))
 @ActiveProfiles("integration")
 class TodoIntegrationTest : IntegrationBaseTest() {
